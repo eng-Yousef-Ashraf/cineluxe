b7957b9f5c856d2529828d0b6fa3954177997e6c
eng:rahma_mahmmoud <mahmmoudrahma9@gmail.com>
2026-05-06 23:15:14 +0300

    feat: implement main layout with bottom navigation bar

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -6,4 +6,5 @@ class AppRoutes {
   static const String onboardingScreen = 'onboarding_screen';
   static const String updateProfileScreen = 'update_profile_screen';
   static const String homeScreen = 'home_screen';
+  static const mainLayout = 'MainLayout';
 }
